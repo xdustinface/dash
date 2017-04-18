e712d8fdad7d2886cd9b275c51b2d9b65fece05c
Russell Yanofsky <russ@yanofsky.org>
2017-04-18 16:42:30 -0400

    Remove direct bitcoin calls from qt transaction table files

--- src/qt/transactionrecord.h
+++ src/qt/transactionrecord.h
@@ -12,8 +12,12 @@
 #include <QList>
 #include <QString>
 
-class CWallet;
-class CWalletTx;
+namespace interface {
+class Node;
+class Wallet;
+struct WalletTx;
+struct WalletTxStatus;
+}
 
 /** UI model for transaction status. The transaction status is the part of a transaction that will change over time.
  */
@@ -126,8 +130,8 @@ public:
 
     /** Decompose CWallet transaction to model transaction records.
      */
-    static bool showTransaction(const CWalletTx &wtx);
-    static QList<TransactionRecord> decomposeTransaction(const CWallet *wallet, const CWalletTx &wtx);
+    static bool showTransaction();
+    static QList<TransactionRecord> decomposeTransaction(const interface::WalletTx& wtx);
 
     /** @name Immutable transaction attributes
       @{*/
@@ -158,11 +162,11 @@ public:
 
     /** Update status from core wallet tx.
      */
-    void updateStatus(const CWalletTx &wtx, int chainLockHeight);
+    void updateStatus(const interface::WalletTxStatus& wtx, int numBlocks, int64_t adjustedTime, int chainLockHeight);
 
     /** Return whether a status update is needed.
      */
-    bool statusUpdateNeeded(int chainLockHeight) const;
+    bool statusUpdateNeeded(int numBlocks, int chainLockHeight) const;
 };
 
 #endif // BITCOIN_QT_TRANSACTIONRECORD_H
