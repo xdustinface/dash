936d38dd43a7a8baa90755b51fba694be7749b1c
xdustinface <xdustinfacex@gmail.com>
2020-06-18 04:31:55 +0200

    qt: Add platform specific css loading to GUIUtil::loadStyleSheet
    This commit leads to GUIUtil::loadStyleSheet treating css code between
    <os="<os_list>"> and </os> different. It will only become added for
    operating systems provided in the list of the sections start tag.
    
    There may be multiple entries per section. Possible entries:
    
    - macosx
    - windows
    - other
    
    <os_list> must be a combination of the three options above separated by
    comma like in "windows,macosx".
    
    Its ok to have multiple <os="...">...</os> sections in a file with
    arbitrary OS combinations. They will all become added to the end of the
    file though. Means even putting an <os> section in the top of the file
    would become appended to the end of the file during loading which should
    be kept in mind when adding sections to avoid unexpected overwriting.
    
    Example
    ------------------------------------------------------------------------
    
    <os="macosx, windows, other">
    
    /* Example section to add styles for all operating systems
       Remove any to exclude it.
    */
    
    </os>

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -6,6 +6,7 @@
 #include <qt/guiutil.h>
 
 #include <qt/bitcoinaddressvalidator.h>
+#include <qt/bitcoingui.h>
 #include <qt/bitcoinunits.h>
 #include <qt/qvalidatedlineedit.h>
 #include <qt/walletmodel.h>
@@ -1057,7 +1058,39 @@ void loadStyleSheet(QWidget* widget, bool fForceUpdate)
         {
             QFile qFile(QString(stylesheetDirectory + "/" + "%1%2").arg(name).arg(isStyleSheetDirectoryCustom() ? ".css" : ""));
             if (qFile.open(QFile::ReadOnly)) {
-                stylesheet->append(QLatin1String(qFile.readAll()));
+                QString strStyle = QLatin1String(qFile.readAll());
+
+                // RegEx to match all <os=...></os> groups in the stylesheet
+                QRegularExpressionMatch osStyleMatch;
+                QRegularExpression osStyleExp("^(<os=(?:'|\").+(?:'|\")>)((?:.|\n)+?)(</os>?)$");
+                osStyleExp.setPatternOptions(QRegularExpression::MultilineOption);
+                QRegularExpressionMatchIterator it = osStyleExp.globalMatch(strStyle);
+
+                // For all <os=...></os> sections
+                while (it.hasNext() && (osStyleMatch = it.next()).isValid()) {
+
+                    QStringList listMatches = osStyleMatch.capturedTexts();
+
+                    // Full match + 3 group matches
+                    if (listMatches.size() % 4) {
+                        qDebug() << "Invalid OS specific stylesheet section.";
+                        return;
+                    }
+
+                    for (int i = 0; i < listMatches.size(); i+=4) {
+                        if (!listMatches[i+1].contains(QString::fromStdString(BitcoinGUI::DEFAULT_UIPLATFORM))) {
+                            // If os is not supported for this styles
+                            // just remove the full match
+                            strStyle.replace(listMatches[i], "");
+                        } else {
+                            // If its supported remove the <os=...></os> tags
+                            strStyle.replace(listMatches[i+1], "");
+                            strStyle.replace(listMatches[i+3], "");
+                        }
+                    }
+                }
+
+                stylesheet->append(strStyle);
             }
         };
 
