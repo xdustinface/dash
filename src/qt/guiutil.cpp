44de8a93f235b7d67c1a154ba1fed2023899ad3a
xdustinface <xdustinfacex@gmail.com>
2020-06-18 04:47:13 +0200

    qt: Remove scrollbar.css and all its remaining mentions

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -98,8 +98,7 @@ static const std::map<QString, QString> mapStyleToTheme{
     {"general.css", ""},
     {"dark.css", "Dark"},
     {"light.css", "Light"},
-    {"traditional.css", "Traditional"},
-    {"scrollbars.css", ""}
+    {"traditional.css", "Traditional"}
 };
 
 /** Font related default values. */
