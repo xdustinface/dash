2e1d2ddb387eaf60e32dbdb554eca27a447201fa
xdustinface <xdustinfacex@gmail.com>
2020-07-22 04:00:36 +0200

    qt: Removes a dot

--- src/qt/receivecoinsdialog.cpp
+++ src/qt/receivecoinsdialog.cpp
@@ -33,7 +33,7 @@ ReceiveCoinsDialog::ReceiveCoinsDialog(QWidget* parent) :
                       ui->label_3}, GUIUtil::FontWeight::Normal, 15);
 
     ui->reqLabel->setPlaceholderText(tr("Enter a label for this request to add its address to your address book"));
-    ui->reqMessage->setPlaceholderText(tr("Enter a message which gets added to this request."));
+    ui->reqMessage->setPlaceholderText(tr("Enter a message which gets added to this request"));
 
     // context menu actions
     QAction *copyURIAction = new QAction(tr("Copy URI"), this);
