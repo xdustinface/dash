02c394c30ac80623fb1935ca010eaf8ae726963c
xdustinface <xdustinfacex@gmail.com>
2020-08-23 11:13:26 +0200

    wallet: Fix signing in CreateTransaction
    Prior it was messed because of 60d96a1a28b55f071c6144f248e136679c44337e. 
    Set coins isn't sorted the same way as txNew.vin is so it sometimes may 
    pick wrong coins for signing the input.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -3820,7 +3820,7 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CWalletT
             bool pick_new_inputs = true;
             CAmount nValueIn = 0;
             CAmount nAmountToSelectAdditional{0};
-            // Start with no fee and loop until there is enough fee, try it 500 times.
+            // Start with no nAmountToSelectAdditional and loop until there is enough to cover the request, try it 500 times.
             int nMaxTries = 500;
             while (--nMaxTries)
             {
@@ -4079,11 +4079,18 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CWalletT
 
         if (sign)
         {
+            auto getScriptPubKey = [&](const CTxIn& txin) -> const CScript& {
+                for (const auto& coin : setCoins) {
+                    if (txin.prevout == coin.outpoint) {
+                        return coin.txout.scriptPubKey;
+                    }
+                }
+                assert(false);
+            };
             CTransaction txNewConst(txNew);
-            int nIn = 0;
-            for(const auto& coin : setCoins)
+            for (int nIn = 0; nIn < txNew.vin.size(); ++nIn)
             {
-                const CScript& scriptPubKey = coin.txout.scriptPubKey;
+                const CScript& scriptPubKey = getScriptPubKey(txNew.vin[nIn]);
                 SignatureData sigdata;
 
                 if (!ProduceSignature(TransactionSignatureCreator(this, &txNewConst, nIn, SIGHASH_ALL), scriptPubKey, sigdata))
@@ -4093,8 +4100,6 @@ bool CWallet::CreateTransaction(const std::vector<CRecipient>& vecSend, CWalletT
                 } else {
                     UpdateTransaction(txNew, nIn, sigdata);
                 }
-
-                nIn++;
             }
         }
 
