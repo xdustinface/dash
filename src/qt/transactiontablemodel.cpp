ec53e705b1e389097a13f6527b504b33f961bf58
xdustinface <xdustinfacex@gmail.com>
2020-09-15 04:06:46 +0200

    qt: Drop "LLMQ based" in some status strings

--- src/qt/transactiontablemodel.cpp
+++ src/qt/transactiontablemodel.cpp
@@ -359,10 +359,10 @@ QString TransactionTableModel::formatTxStatus(const TransactionRecord *wtx) cons
     }
 
     if ((wtx->status.status == TransactionStatus::Unconfirmed || wtx->status.status == TransactionStatus::Confirming) && wtx->status.lockedByInstantSend) {
-        status += " (" + tr("verified via LLMQ based InstantSend") + ")";
+        status += " (" + tr("verified via InstantSend") + ")";
     }
     if ((wtx->status.status == TransactionStatus::Confirming || wtx->status.status == TransactionStatus::Confirmed) && wtx->status.lockedByChainLocks) {
-        status += " (" + tr("locked via LLMQ based ChainLocks") + ")";
+        status += " (" + tr("locked via ChainLocks") + ")";
     }
 
     return status;
