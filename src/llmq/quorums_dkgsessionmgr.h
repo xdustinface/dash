aeec4fc038523869347fe07d06a4031327c04cf3
xdustinface <xdustinfacex@gmail.com>
2020-12-18 20:33:39 +0100

    llmq: Add CDKGSessionManager::WriteEncryptedContributions
    Allows to store each member's encrypted contributions of the DKG.

--- src/llmq/quorums_dkgsessionmgr.h
+++ src/llmq/quorums_dkgsessionmgr.h
@@ -61,10 +61,11 @@ public:
     bool GetJustification(const uint256& hash, CDKGJustification& ret) const;
     bool GetPrematureCommitment(const uint256& hash, CDKGPrematureCommitment& ret) const;
 
-    // Verified contributions are written while in the DKG
+    // Contributions are written while in the DKG
     void WriteVerifiedVvecContribution(Consensus::LLMQType llmqType, const CBlockIndex* pindexQuorum, const uint256& proTxHash, const BLSVerificationVectorPtr& vvec);
     void WriteVerifiedSkContribution(Consensus::LLMQType llmqType, const CBlockIndex* pindexQuorum, const uint256& proTxHash, const CBLSSecretKey& skContribution);
     bool GetVerifiedContributions(Consensus::LLMQType llmqType, const CBlockIndex* pindexQuorum, const std::vector<bool>& validMembers, std::vector<uint16_t>& memberIndexesRet, std::vector<BLSVerificationVectorPtr>& vvecsRet, BLSSecretKeyVector& skContributionsRet);
+    void WriteEncryptedContributions(Consensus::LLMQType llmqType, const CBlockIndex* pindexQuorum, const uint256& proTxHash, const CBLSIESMultiRecipientObjects<CBLSSecretKey>& contributions);
 
 private:
     void CleanupCache();
