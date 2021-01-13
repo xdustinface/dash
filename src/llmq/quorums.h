4a1c8333187c5b1ea7280819ffe2e94452fe4118
xdustinface <xdustinfacex@gmail.com>
2021-01-13 03:31:48 +0100

    llmq: Introduce CQuorumDataRequest as wrapper for QGETDATA requests

--- src/llmq/quorums.h
+++ src/llmq/quorums.h
@@ -23,6 +23,106 @@ namespace llmq
 class CDKGSessionManager;
 
 /**
+ * An object of this class represents a QGETDATA request or a QDATA response header
+ */
+class CQuorumDataRequest
+{
+public:
+
+    enum Flags : uint16_t {
+        QUORUM_VERIFICATION_VECTOR = 0x0001,
+        ENCRYPTED_CONTRIBUTIONS = 0x0002,
+    };
+    enum Errors : uint8_t {
+        NONE = 0x00,
+        QUORUM_TYPE_INVALID = 0x01,
+        QUORUM_BLOCK_NOT_FOUND = 0x02,
+        QUORUM_NOT_FOUND = 0x03,
+        MASTERNODE_IS_NO_MEMBER = 0x04,
+        QUORUM_VERIFICATION_VECTOR_MISSING = 0x05,
+        ENCRYPTED_CONTRIBUTIONS_MISSING = 0x06,
+        UNDEFINED = 0xFF,
+    };
+
+private:
+    Consensus::LLMQType llmqType;
+    uint256 quorumHash;
+    uint16_t nDataMask;
+    uint256 proTxHash;
+    Errors nError;
+
+    int64_t nTime;
+    bool fProcessed;
+
+    static const int64_t nExpirySeconds{300};
+
+public:
+
+    CQuorumDataRequest() : nTime(GetAdjustedTime()) {}
+    CQuorumDataRequest(const Consensus::LLMQType llmqTypeIn, const uint256& quorumHashIn, const uint16_t nDataMaskIn, const uint256& proTxHashIn = uint256()) :
+        llmqType(llmqTypeIn),
+        quorumHash(quorumHashIn),
+        nDataMask(nDataMaskIn),
+        proTxHash(proTxHashIn),
+        nError(UNDEFINED),
+        nTime(GetAdjustedTime()),
+        fProcessed(false) {}
+
+    ADD_SERIALIZE_METHODS
+
+    template <typename Stream, typename Operation>
+    inline void SerializationOp(Stream& s, Operation ser_action)
+    {
+        READWRITE(llmqType);
+        READWRITE(quorumHash);
+        READWRITE(nDataMask);
+        READWRITE(proTxHash);
+        if (ser_action.ForRead()) {
+            try {
+                READWRITE(nError);
+            } catch (...) {
+                nError = UNDEFINED;
+            }
+        } else if (nError != UNDEFINED) {
+            READWRITE(nError);
+        }
+    }
+
+    const Consensus::LLMQType GetLLMQType() const { return llmqType; }
+    const uint256& GetQuorumHash() const { return quorumHash; }
+    const uint16_t GetDataMask() const { return nDataMask; }
+    const uint256& GetProTxHash() const { return proTxHash; }
+
+    void SetError(Errors nErrorIn) { nError = nErrorIn; }
+    const Errors GetError() const { return nError; }
+
+    bool IsExpired() const
+    {
+        return (GetAdjustedTime() - nTime) >= nExpirySeconds;
+    }
+    bool IsProcessed() const
+    {
+        return fProcessed;
+    }
+    void SetProcessed()
+    {
+        fProcessed = true;
+    }
+
+    bool operator==(const CQuorumDataRequest& other)
+    {
+        return llmqType == other.llmqType &&
+               quorumHash == other.quorumHash &&
+               nDataMask == other.nDataMask &&
+               proTxHash == other.proTxHash;
+    }
+    bool operator!=(const CQuorumDataRequest& other)
+    {
+        return !(*this == other);
+    }
+};
+
+/**
  * An object of this class represents a quorum which was mined on-chain (through a quorum commitment)
  * It at least contains information about the members and the quorum public key which is needed to verify recovered
  * signatures from this quorum.
@@ -116,5 +216,7 @@ private:
 extern CQuorumManager* quorumManager;
 
 } // namespace llmq
+
+template<> struct is_serializable_enum<llmq::CQuorumDataRequest::Errors> : std::true_type {};
 
 #endif // BITCOIN_LLMQ_QUORUMS_H
